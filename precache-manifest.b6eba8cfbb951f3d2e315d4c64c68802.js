1ab0f4eafbda1c7f6bef2e9a03565a09ccaa98f9
gaoyang <2313332818@qq.com>
2020-02-06 23:23:50 +0800

    website2.0 finish

---
+++ precache-manifest.b6eba8cfbb951f3d2e315d4c64c68802.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98b3c8db9b1de411248c29af580cca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f592a85c43a5abf75b9",
+    "url": "/static/css/main.1cfd2bd0.chunk.css"
+  },
+  {
+    "revision": "4c1cdb52ca8f6b140326",
+    "url": "/static/js/2.b9efa1bb.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.b9efa1bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f592a85c43a5abf75b9",
+    "url": "/static/js/main.9e10dd90.chunk.js"
+  },
+  {
+    "revision": "43ca0a99945f71264764",
+    "url": "/static/js/runtime-main.58b10c31.js"
+  },
+  {
+    "revision": "397a21dfd602f4ba7ce1ecc26b085b62",
+    "url": "/static/media/DeFi-Full-Stacks.397a21df.svg"
+  },
+  {
+    "revision": "208bb0d261789902cc8e61f048f3637a",
+    "url": "/static/media/alpha.208bb0d2.svg"
+  },
+  {
+    "revision": "ceadf4deb2b2e431feec393426098efd",
+    "url": "/static/media/arrow-right.ceadf4de.svg"
+  },
+  {
+    "revision": "f32ad1edde617914688a49855a70900d",
+    "url": "/static/media/axonomy.f32ad1ed.svg"
+  },
+  {
+    "revision": "130a2a0e4d5f50f62113b0a7c0a7657b",
+    "url": "/static/media/bg-circle.130a2a0e.png"
+  },
+  {
+    "revision": "412458fed87924e06ed3beb8d5ded08d",
+    "url": "/static/media/bitpie.412458fe.png"
+  },
+  {
+    "revision": "62e988a26d929fdad7ec1dd39d6db2a2",
+    "url": "/static/media/circle.62e988a2.png"
+  },
+  {
+    "revision": "7908edd76da13f08e0f316d0447bbcf7",
+    "url": "/static/media/hashbot.7908edd7.png"
+  },
+  {
+    "revision": "4baaceb6842f416b07ca31ca52a386d1",
+    "url": "/static/media/hotbit.4baaceb6.png"
+  },
+  {
+    "revision": "2372c3caaf2f4c9bd7427cf93d01f222",
+    "url": "/static/media/huobi.2372c3ca.png"
+  },
+  {
+    "revision": "9ca5186d8b417106fb35f8a4e4f53d70",
+    "url": "/static/media/icon-composability.9ca5186d.svg"
+  },
+  {
+    "revision": "14b7c0c374f011bacf0377c55aa70156",
+    "url": "/static/media/icon-contracts.14b7c0c3.svg"
+  },
+  {
+    "revision": "efd5a50f0ed872964b380bb840d658c9",
+    "url": "/static/media/icon-cost.efd5a50f.svg"
+  },
+  {
+    "revision": "807ae6cdb1138b7c291640b71e3c854c",
+    "url": "/static/media/icon-decentralization.807ae6cd.png"
+  },
+  {
+    "revision": "cac2a5d477434464dffe30803f934ebd",
+    "url": "/static/media/icon-down.cac2a5d4.svg"
+  },
+  {
+    "revision": "c60e8c3d6357930041f2fc1d75345816",
+    "url": "/static/media/icon-medium.c60e8c3d.svg"
+  },
+  {
+    "revision": "47b494563b09334a304acd8009a566d6",
+    "url": "/static/media/icon-non-custody.47b49456.svg"
+  },
+  {
+    "revision": "98b65c08b72ede4b4601c5483acd9edc",
+    "url": "/static/media/icon-permission.98b65c08.svg"
+  },
+  {
+    "revision": "337a5c535ae3b5566214504d7f7c052d",
+    "url": "/static/media/icon-telegram.337a5c53.svg"
+  },
+  {
+    "revision": "127bc38812bfe90391c9c5d329447659",
+    "url": "/static/media/icon-twitter.127bc388.svg"
+  },
+  {
+    "revision": "b1917f0cde69d07f140b7ef4e169b103",
+    "url": "/static/media/icon-up.b1917f0c.svg"
+  },
+  {
+    "revision": "416b74ce8d1b0d69f4afabd9e09b36e5",
+    "url": "/static/media/imtoken.416b74ce.png"
+  },
+  {
+    "revision": "4fccc6f0cd380eb49d178af93854246f",
+    "url": "/static/media/litex.4fccc6f0.svg"
+  },
+  {
+    "revision": "a98fd76c65dd20b6cfa84032b8dde53a",
+    "url": "/static/media/logo-USDx.a98fd76c.svg"
+  },
+  {
+    "revision": "71539d55b1bff747e517c08fdd5a2aad",
+    "url": "/static/media/logo-X-Swap.71539d55.svg"
+  },
+  {
+    "revision": "aaf8ecf52d941375205a79a17178b916",
+    "url": "/static/media/logo_LENDF.aaf8ecf5.svg"
+  },
+  {
+    "revision": "76c57180665941f259bd2240f2eab851",
+    "url": "/static/media/math.76c57180.png"
+  },
+  {
+    "revision": "d015abbc2074abc380ac8faeced706b8",
+    "url": "/static/media/nvex.d015abbc.svg"
+  },
+  {
+    "revision": "aef76fd348f68e6b42cadae600c7db94",
+    "url": "/static/media/portto.aef76fd3.svg"
+  },
+  {
+    "revision": "a35cf90e6954ab3c306f7c1d892c3b2c",
+    "url": "/static/media/slogen-cn.a35cf90e.png"
+  },
+  {
+    "revision": "df36a1336457ba6f8777fce982b36fec",
+    "url": "/static/media/slogen-en.df36a133.png"
+  },
+  {
+    "revision": "bf72a8ae6208dd68f0400e7b9cb5d858",
+    "url": "/static/media/steaker.bf72a8ae.svg"
+  },
+  {
+    "revision": "d2c0f11f078f5cc41f3a499ef5f536ed",
+    "url": "/static/media/swap.d2c0f11f.svg"
+  },
+  {
+    "revision": "d90d5ee19a15dca8961b875f534514fb",
+    "url": "/static/media/tokenlon.d90d5ee1.jpg"
+  },
+  {
+    "revision": "df0b05f4d587568d5e876dcc95cc760e",
+    "url": "/static/media/trust.df0b05f4.png"
+  },
+  {
+    "revision": "bc9fd62771528309534176edcc0b03af",
+    "url": "/static/media/twopoint.bc9fd627.png"
+  },
+  {
+    "revision": "b2071e21f2fb91363c80d3034b810f24",
+    "url": "/static/media/wallet.b2071e21.png"
+  },
+  {
+    "revision": "e7214f943657dd268566c59f40171867",
+    "url": "/static/media/zhaoshang.e7214f94.png"
+  }
+]);
