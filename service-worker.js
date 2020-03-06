b4130d00d5c93afd575527a36e0f706d85e06216
gaoyang <gy@gmail.com>
2020-03-06 19:04:10 +0800

    update web (mobile)

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3f2a40f240c93aefd60c9f0f386c8159.js"
+  "/precache-manifest.e7584e14470e97e015a6cc05be54c7e3.js"
 );
 
 self.addEventListener('message', (event) => {
