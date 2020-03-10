bf17ad1516b2cada5d5ab48d38e8058c67ad5c4a
gaoyang <gy@gmail.com>
2020-03-10 12:10:24 +0800

    add google code

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47ceb62b38fc15e0f0becf5bdcc52d99.js"
+  "/precache-manifest.8962191d4105863c3ff6dad293d18391.js"
 );
 
 self.addEventListener('message', (event) => {
