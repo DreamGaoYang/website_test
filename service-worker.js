088e331bf2f0ed44de266befb9e892f8f5fc8be8
gaoyang <gy@gmail.com>
2021-03-01 19:12:00 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea0d1f15173a818e70ac4754c6df29e3.js"
+  "/precache-manifest.d126a3da6da38ef9b237a98582a32337.js"
 );
 
 self.addEventListener('message', (event) => {
